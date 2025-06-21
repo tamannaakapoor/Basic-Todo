176712de9ff1621149b40d691d695b470273aaf1
tamannaakapoor <kapoortamanna1805@gmail.com>
2025-06-22 05:07:59 +0530

    added css

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -3,8 +3,11 @@ import React from 'react'
 export const About = () => {
   return (
     <div>
-        This is an about component
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. Quisquam, voluptates.</p>
+      <strong>MyTodosList</strong> is a simple and elegant task management tool
+        designed to help you stay organized, focused, and productive. Whether you're
+        managing your daily chores, study plans, or long-term goals, this app is your
+        perfect pastel-colored companion.
+        
     </div>
   )
 }
